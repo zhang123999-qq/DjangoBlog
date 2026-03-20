3280afe563e88beffe62ff41695cce5fe5e0fd7c
zhang123999-qq <2973084264@qq.com>
2026-03-20 14:53:58 +0800

    添加局域网访问支持

---
+++ run_lan.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ========================================
+echo   DjangoBlog 局域网启动脚本
+echo ========================================
+echo.
+echo 启动服务并允许局域网访问...
+echo 访问地址: http://你的IP:18789
+echo.
+python manage.py runserver 0.0.0.0:18789
