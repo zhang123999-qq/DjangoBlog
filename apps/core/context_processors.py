7ac538c83fed2acb8798dd6082636b2dcdf4d176
zhang123999-qq <2973084264@qq.com>
2026-03-21 15:14:40 +0800

    fix: 修复安装向导在数据库表不存在时报错的问题
    - context_processors.py: 捕获 OperationalError 异常
    - 安装过程中数据库表不存在时返回 None 而不是抛出异常

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -1,8 +1,15 @@
-from .models import SiteConfig
+from django.db.utils import OperationalError, ProgrammingError
 
 
 def site_config(request):
     """提供 site_config 给全局模板"""
-    return {
-        'site_config': SiteConfig.get_solo()
-    }
+    try:
+        from .models import SiteConfig
+        return {
+            'site_config': SiteConfig.get_solo()
+        }
+    except (OperationalError, ProgrammingError):
+        # 数据库表不存在（安装过程中）
+        return {
+            'site_config': None
+        }
